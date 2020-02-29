d9eac6c6659cf5cdf9863f43aa3701051d1aa3e8
Matt Brierley <matt.brierley86@gmail.com>
2020-02-28 23:49:58 -0800

    Player now rotates toward targeted enemy

---
+++ Assets/Scripts/FaceEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceEnemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private FindNearestEnemy nearestEnemy;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        nearestEnemy = gameObject.GetComponent<FindNearestEnemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(nearestEnemy.nearestEnemy)
+        {
+            Vector2 enemyPos = nearestEnemy.nearestEnemy.transform.position;
+            Vector2 lookDir = enemyPos - rb.position;
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+        }
+    }
+}
