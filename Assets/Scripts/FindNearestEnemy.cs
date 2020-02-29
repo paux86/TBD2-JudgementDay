4a6ebd3f355f552b57ee8e5f87d2806b4b24f722
Matt Brierley <matt.brierley86@gmail.com>
2020-02-28 19:58:46 -0800

    Added Enemy tag and FindNearestEnemy script

---
+++ Assets/Scripts/FindNearestEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindNearestEnemy : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        findNearest();
+    }
+
+    void findNearest()
+    {
+        float shortestDistanceToEnemy = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach(GameObject currentEnemy in enemies)
+        {
+            float distanceToEnemy = (currentEnemy.transform.position - gameObject.transform.position).sqrMagnitude;
+            if(distanceToEnemy < shortestDistanceToEnemy)
+            {
+                shortestDistanceToEnemy = distanceToEnemy;
+                nearestEnemy = currentEnemy;
+            }
+        }
+
+        if(nearestEnemy)
+            Debug.DrawLine(gameObject.transform.position, nearestEnemy.transform.position);
+    }
+}
