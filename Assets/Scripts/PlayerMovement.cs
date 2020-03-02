14b98cf4fcdfa9750d78b3989baa5ad6728d7152
Cody <cstumbough@gmail.com>
2020-03-02 12:12:49 -0800

    Added PlayerMovement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 18f;
+    [SerializeField] float screenHeightInUnits = 32f;
+    [SerializeField ]public float speed = 25f;
+    private Vector2 destination = new Vector2(9, 16);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed *= Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            float mouseXPosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+            float mouseYPosInUnits = Input.mousePosition.y / Screen.height * screenHeightInUnits;
+            destination = new Vector2(mouseXPosInUnits, mouseYPosInUnits);
+        }
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destination, speed);
+    }
+}
