368a7c4f942dddf8ef08b3d316991703ad734d3d
Matt Brierley <matt.brierley86@gmail.com>
2020-03-07 10:28:17 -0800

    Added simple boss animations and AI

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+    [SerializeField] HealthBar healthBar;
+
+    private void Start()
+    {
+        healthBar.SetMaxHealth(health);
+    }
+
+    public void TakeDamage(int attackDamage)
+    {
+        health -= attackDamage;
+        healthBar.SetHealth(health);
+    }
+}
