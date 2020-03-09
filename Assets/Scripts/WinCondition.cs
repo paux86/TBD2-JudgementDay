0c1dddb9b1482282d7da17375ed7d3d391d98b35
Malakai Johnson <malakai.johnson642@gmail.com>
2020-03-09 12:30:00 -0700

    began implementing Win Condition with placeholder for interfacing with Wave Spawner

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    bool isWon;
+
+    void Start(){
+        isWon = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(wave.isFinished && GameObject.FindGameObjectsWithTag("Enemy").Length == 0) //wave.isFinished placeholder for implementation with Wave Spawner
+        {
+            isWon = true;
+        }
+    }
+}
