bd495afd78ec4e039a961c3c1fd67a4129c8cd49
Cody <cstumbough@gmail.com>
2020-03-09 11:48:39 -0700

    Added maxHealth and rudimentary weapon inventory

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -4,37 +4,68 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    [SerializeField] private int health = 100;
+    [SerializeField] private int maxHealth = 100;
     [SerializeField] private int moneyCount = 0;
     [SerializeField] private float armor = 0;
+    [SerializeField] private Weapon currentWeapon;
+    [SerializeField] private Weapon[] weaponInventory = new Weapon[3];
+    public int health;
 
-    public int getHealth()
+    void Start()
+    {
+        health = maxHealth;
+        currentWeapon = weaponInventory[0];
+    }
+
+    public int GetHealth()
     {
         return health;
     }
 
-    public int getMoneyCount()
+    public int GetMoneyCount()
     {
         return moneyCount;
     }
 
-    public float getArmor()
+    public float GetArmor()
     {
         return armor;
     }
 
-    public void incrementHealth(int number)
+    public Weapon GetCurrentWeapon()
+    {
+        return currentWeapon;
+    }
+
+    public void IncrementHealth(int number)
     {
         health += number;
     }
 
-    public void incrementMoney(int number)
+    public void IncrementMoney(int number)
     {
         moneyCount += number;
     }
 
-    public void setArmor(float number)
+    public void SetArmor(float number)
     {
         armor = number;
+    }
+
+    public void AddWeapon(Weapon newWeapon)
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if(currentWeapon == weaponInventory[i])
+            {
+                weaponInventory[i] = newWeapon;
+                ChangeWeapon(i);
+            }
+        }
+    }
+
+    public void ChangeWeapon(int numOfWeapon)
+    {
+        currentWeapon = weaponInventory[numOfWeapon];
     }
 }
