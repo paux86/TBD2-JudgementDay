ffc9cf58ebd3ef267a89ca6371ca4e933fc33607
Cody <cstumbough@gmail.com>
2020-03-11 11:51:27 -0700

    Fixed movement: collisions working and sliding removed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
-    Collider2D cd;
-    [SerializeField] public float screenWidthInUnits = 5.4f;
-    [SerializeField] public float screenHeightInUnits = 9.6f;
     [SerializeField] public float playerSpeed = 5f;
-    private Vector3 destination = new Vector2(0, 0);
-    private Vector3 tempVect;
+    private Vector3 destination;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        cd = gameObject.GetComponent<Collider2D>();
-        //playerSpeed *= Time.deltaTime;
+        destination = transform.position;
     }
 
     // Update is called once per frame
@@ -25,22 +18,14 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            rb.velocity = Vector2.zero;
-            rb.angularVelocity = 0f;
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log("Destination: " + destination);
-            tempVect = destination - transform.position;
-            Debug.Log("TempVect: " + tempVect);
+            destination.z = transform.position.z;
         }
         Move();
     }
 
     private void Move()
     {
-        //transform.position = Vector2.MoveTowards(transform.position, destination, playerSpeed);
-        rb.MovePosition(rb.transform.position + tempVect * (playerSpeed * Time.deltaTime));
-        
-        //rb.MovePosition(destination.normalized + tempVect);
-        Debug.Log("MovePosition: " + (rb.transform.position + tempVect));
+        transform.position = Vector3.MoveTowards(transform.position, destination, playerSpeed * Time.deltaTime);
     }
 }
