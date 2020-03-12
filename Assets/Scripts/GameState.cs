403afe8d88fd2269e51594272931d42fe8cfb124
Joshua Gottlieb <joshua.gottlieb824@myci.csuci.edu>
2020-03-11 21:49:26 -0700

    added asset and some code

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+   
+    [SerializeField] int numLevels;
+    [SerializeField] bool[] levelsComplete;
+
+
+    private void Start()
+    {
+        levelsComplete = new bool[numLevels];
+    }
+
+    private void Awake()
+    {
+        int gameStateCount = FindObjectsOfType<GameState>().Length;
+        if (gameStateCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
