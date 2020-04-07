b65b2a5412cb5be49482793d9db3ea4a7cae7e4e
Matt Brierley <matt.brierley86@gmail.com>
2020-04-06 21:49:16 -0700

    Repurposed weapon swap button to inventory button, added inventory handler

---
+++ Assets/Scripts/InventoryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryHandler : MonoBehaviour
+{
+    public GameObject[] inventoryLists;
+    private bool isOpen = false;
+
+    public void ToggleInventory()
+    {
+        isOpen = !isOpen;
+
+        if (inventoryLists.Length > 0)
+        {
+            foreach (GameObject  list in inventoryLists)
+            {
+                list.SetActive(isOpen);
+            }
+        }
+
+        if (Time.timeScale == 0)
+            Time.timeScale = 1;
+        else
+            Time.timeScale = 0;
+    }
+}
