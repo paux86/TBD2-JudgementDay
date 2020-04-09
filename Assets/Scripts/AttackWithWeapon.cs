b5b3680cfd44938f6c6a5b600c952a54395ba7f5
Matt Brierley <matt.brierley86@gmail.com>
2020-04-08 22:24:42 -0700

    Fixed persistent line renderer on swapping away from hitscan

--- Assets/Scripts/AttackWithWeapon.cs
+++ Assets/Scripts/AttackWithWeapon.cs
@@ -57,7 +57,7 @@ public class AttackWithWeapon : MonoBehaviour
             }
             else
             {
-                
+                lineRenderer.enabled = false;
                 GameObject projectileObject = Instantiate(equippedWeapon.projectile, firePoint.position, firePoint.rotation);
                 Projectile projectile =  projectileObject.GetComponent<Projectile>();
                 projectile.SetDamage(equippedWeapon.attackDamage);
