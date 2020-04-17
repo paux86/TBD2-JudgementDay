59f51a7624e8635c2f974d6fffb35a3459d5bc5d
Malakai Johnson <malakai.johnson642@gmail.com>
2020-04-17 14:47:27 -0700

    Added shop scene with basic elements, not yet functional.

---
+++ Assets/Scripts/ItemStall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemStall : MonoBehaviour
+{
+   [SerializeField] public UsableItem item;
+
+    private void Start(){
+        gameObject.getComponent<Image>.sprite = item.buttonSprite;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ShopKeeper shopKeeper = FindObjectOfType<shopKeeper>();
+        shopKeeper.purchaseItem(item);
+    }
+
+}
