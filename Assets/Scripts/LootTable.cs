ed8242cd487f4762d5e155b511c1ab3e527669f8
Cody <Cstumbough@gmail.com>
2020-04-19 09:49:53 -0700

    Removed Debug Log messages about loot

--- Assets/Scripts/LootTable.cs
+++ Assets/Scripts/LootTable.cs
@@ -16,34 +16,22 @@ public class LootTable : ScriptableObject
     public int ChooseDrop()
     {
         rolledValue = UnityEngine.Random.Range(0, totalWeight);
-        Debug.Log("Rolled Random Value: " + rolledValue);
         for(int i=0;i<Weights.Count;i++)
         {
-            Debug.Log("Loop #: " + (i + 1) + " First Check - Comparing " + rolledValue + " to " + Weights[i]);
             if(rolledValue <= Weights[i])
             {
-                Debug.Log("Loop #: " + (i + 1) + " Second Check - Is Element " + i + " enabled?");
                 if(Enabled[i] && rolledValue > -1)
                 {
-                    //////////////////////////////////////////////////////////
-                    Debug.Log("Loop #: " + (i + 1) + " Dropping: " + i);
                     return i;
-                    //////////////////////////////////////////////////////////
                 }
                 else
                 {   
-                    Debug.Log("Loop #: " + (i + 1) + " Element " + i + " is not enabled or " + rolledValue + " <= -1");
-                    rolledValue = -1;
-                    Debug.Log("Loop #: " + (i + 1) + " Setting rolled value to -1");
                     return -1;
                 }
             }
-            else if(rolledValue != -1)
+            else if(rolledValue > Weights[i])
             {
-                Debug.Log("Loop #: " + (i + 1) + " Else - Rolled Value: " + rolledValue);
-                Debug.Log("Loop #: " + (i + 1) + " Else - Subtracting " + rolledValue + " by " + Weights[i]);
                 rolledValue -= Weights[i];
-                Debug.Log("Loop #: " + (i + 1) + " Else - New Rolled Value: " + rolledValue);
             }
         }
         return -1;
