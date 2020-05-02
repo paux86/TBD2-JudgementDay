13f426efaecdee7601a7d6141e262fa32721998a
Malakai Johnson <malakai.johnson642@gmail.com>
2020-05-01 17:21:39 -0700

    Added currentWeaponSlot and maxHealth to persistentStats

--- Assets/Scripts/PersistentStats.cs
+++ Assets/Scripts/PersistentStats.cs
@@ -6,7 +6,11 @@ public class PersistentStats : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] public int moneyCount = 0;
+    [SerializeField] public int maxHealth = 100;
+    [SerializeField] public int currentWeaponSlot;
 
+
+    //These arrays are private and accessed by the getter functions to ensure playerStats gets a copy of the arrays not a reference to them
     [SerializeField] private Weapon[] weaponInventory = new Weapon[3];
     [SerializeField] private UsableItem[] itemInventory = new UsableItem[3];
 
@@ -15,6 +19,8 @@ public class PersistentStats : MonoBehaviour
     public void updateStats(){
         playerStats = FindObjectOfType<PlayerStats>().GetComponent<PlayerStats>();
         this.moneyCount = playerStats.GetMoneyCount();
+        this.maxHealth = playerStats.GetMaxHealth();
+        this.currentWeaponSlot = playerStats.GetCurrentWeaponSlot();
 
         this.weaponInventory = playerStats.GetWeaponInventory();
         this.itemInventory = playerStats.GetItemInventory();
