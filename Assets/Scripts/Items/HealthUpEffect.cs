75a25b28fb933aed8f04dac24e4250c1f0788888
Malakai Johnson <malakai.johnson642@gmail.com>
2020-05-09 11:34:44 -0700

    Created Items which increase Max Health

---
+++ Assets/Scripts/Items/HealthUpEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New HealthUp Effect", menuName = "Item Effects/HealthUp Effect")]
+public class HealthUpEffect : UsableItemEffect
+{
+    public int HealthAmount;
+    public override void ExecuteEffect(UsableItem parentItem, PlayerStats playerStats)
+    {
+        playerStats.IncrementHealthMax(HealthAmount);
+    }
+}
